434a194566be895def0a4b71f90c1f1abc7e0f09
Mrtn Schndlr <mrtnschndlr@gmail.com>
2022-01-24 09:19:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ export const OPENING_HOURS = ''
     + 'Mo,Tu,Th,Fr 09:00-12:00,15:00-18:00,Sa 09:00-12:00;' // regular opening hours
     + 'PH closed;' // closed on public holidays
     + 'Dec 24-Feb Th[1] closed;' // closed in winter times from christmas until first thursday in february
+    + 'Feb Th[1]-Mar Th[1] Mo,Tu closed;' // from first thursday in february until first thursday in march open th,fr,sa only
     + 'Oct Mo[3] closed "Kirchweihmontag";' // church fair every third monday in october
 
 export const COUNTRY_CODE = 'de';
